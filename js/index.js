2b0e598170d73a348111bc6b6338c73d8a347e81
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-06-04 20:18:09 -0400

    Deleting task is next

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // DOM elementts to integrate 
 const inputTask = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
+const deleteTaskButton = document.getElementById("deleteTaskButton");
 const taskList = document.getElementById("taskList");
 
 // Function to add a task
@@ -15,7 +16,13 @@ const itemAdd = inputTask.value;
     inputTask.value = "";
 }
 
+const deleteTask = () => {
+    listItems.textContent = "";
+    listItems.classList.remove("list-items");
+}
+
 addTask();
 
 // Event listener for the add task button
 addTaskButton.addEventListener("click", addTask);
+deleteTaskButton.addEventListener("click", deleteTask);
